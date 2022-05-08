1c941a206a530c21dd85cd4c828018a0ad06e90b
vigneshselva0010 <104215280+vigneshselva0010@users.noreply.github.com>
2022-05-08 13:02:51 +0530

    my first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const calc = require("./user_modules/calculetor");
+
+let a = 10;
+let b = 20;
+console.log("Add : "+calc.add(a,b))
+console.log("Sub : "+calc.sub(a,b))
+console.log("mul : "+calc.mul(a,b))
+console.log("div : "+calc.div(a,b))
+
+console.log("function end");
